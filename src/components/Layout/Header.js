67fd7ca357a4d59b585956bc1f62289c97b02703
Brian <briansmith@rocketcentral.com>
2023-01-30 12:58:26 -0500

    Header component working

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,17 @@
-const Header = props => {
-return (
+import { Fragment } from 'react';
+import classes from './Header.module.scss';
 
-)};
+import mealsImage from '../../assets/meals.jpg';
+
+const Header = props => {
+return <Fragment>
+    <header className={classes.header}>
+        <h1>Reacting to Hunger</h1>
+        <div>
+            <img src={mealsImage} alt="Example of a delicious meal" />
+        </div>
+    </header>
+</Fragment>
+};
 
 export default Header;
