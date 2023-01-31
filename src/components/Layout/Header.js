b72a51af450603dbe0273fac72c23763cc4eb404
Brian <briansmith@rocketcentral.com>
2023-01-31 10:06:11 -0500

    Cart button added to header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,21 @@
 import { Fragment } from 'react';
-import classes from './Header.module.scss';
 
+import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
 
-const Header = props => {
-return <Fragment>
-    <header className={classes.header}>
-        <h1>Reacting to Hunger</h1>
-        <div>
-            <img src={mealsImage} alt="Example of a delicious meal" />
-        </div>
-    </header>
-</Fragment>
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
